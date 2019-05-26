04028bf4b7bb7afddc6ce9c909aade46bb4b5176
Oscar Aguayo <43186229+DcOscar@users.noreply.github.com>
2019-05-26 13:23:19 -0400

    Implementación test Baraka Photos

--- test/logico/GestionadorClubTest.java
+++ test/logico/GestionadorClubTest.java
@@ -19,5 +19,96 @@ import static org.junit.Assert.*;
 public class GestionadorClubTest {
     
 
+    @Test
+    public void comprobarPosibilidadOrden() {
+        GestionadorClub gestionador = new GestionadorClub();
+        double[] grupoDorado = {1, 2, 3, 4};
+        double[] grupoAzul = {2, 3, 4, 5};
+        for (int i = 0; i < grupoDorado.length; i++) {
+            gestionador.agregarBoxeadoraGrupoDorado(grupoDorado[i]);
+            gestionador.agregarBoxeadoraGrupoAzul(grupoAzul[i]);
+        }
+        String resultado = gestionador.ordenarBoxeadoras();
+        String esperado = "Yes";
+        assertEquals(resultado, esperado);
+        System.out.println(resultado);
+    }
+
+    @Test
+    public void comprobarPosibilidadOrden2() {
+        GestionadorClub gestionador = new GestionadorClub();
+        double[] grupoDorado = {1, 5, 4};
+        double[] grupoAzul = {2, 3, 4};
+        for (int i = 0; i < grupoDorado.length; i++) {
+            gestionador.agregarBoxeadoraGrupoDorado(grupoDorado[i]);
+            gestionador.agregarBoxeadoraGrupoAzul(grupoAzul[i]);
+        }
+        String resultado = gestionador.ordenarBoxeadoras();
+        String esperado = "No";
+        assertEquals(resultado, esperado);
+        System.out.println(resultado);
+    }
+
+    @Test
+    public void comprobarPosibilidadOrden3() {
+        GestionadorClub gestionador = new GestionadorClub();
+        double[] grupoDorado = {1.68, 1.79, 1.87, 1.63, 1.73, 1.92};
+        double[] grupoAzul = {1.89, 1.67, 1.80, 1.81, 1.77, 1.94};
+        for (int i = 0; i < grupoDorado.length; i++) {
+            gestionador.agregarBoxeadoraGrupoDorado(grupoDorado[i]);
+            gestionador.agregarBoxeadoraGrupoAzul(grupoAzul[i]);
+        }
+        String resultado = gestionador.ordenarBoxeadoras();
+        String esperado = "Yes";
+        assertEquals(resultado, esperado);
+        System.out.println(resultado);
+    }
+
+    @Test
+    public void comprobarPosibilidadOrden4() {
+        GestionadorClub gestionador = new GestionadorClub();
+        double[] grupoDorado = {1.78, 1.63, 1.84};
+        double[] grupoAzul = {1.67, 1.58, 1.82};
+        for (int i = 0; i < grupoDorado.length; i++) {
+            gestionador.agregarBoxeadoraGrupoDorado(grupoDorado[i]);
+            gestionador.agregarBoxeadoraGrupoAzul(grupoAzul[i]);
+        }
+        String resultado = gestionador.ordenarBoxeadoras();
+        String esperado = "No";
+        assertEquals(resultado, esperado);
+        System.out.println(resultado);
+    }
+
+    @Test
+    public void comprobarCasoAlturaInvalida() {
+        GestionadorClub gestionador = new GestionadorClub();
+        double[] grupoDorado = {0, 0, 0, 0};
+        double[] grupoAzul = {0, 0, 0, 0};
+        for (int i = 0; i < grupoDorado.length; i++) {
+            gestionador.agregarBoxeadoraGrupoDorado(grupoDorado[i]);
+            gestionador.agregarBoxeadoraGrupoAzul(grupoAzul[i]);
+        }
+        boolean condicion = false;
+        if (gestionador.getGrupoAzul().isEmpty() && gestionador.getGrupoDorado().isEmpty()) {
+            condicion = true;
+
+        }
+        assertTrue(condicion);
+    }
+
+    @Test
+    public void comprobarCasoAlturasNoEquitativas() {
+        GestionadorClub gestionador = new GestionadorClub();
+        double[] grupoDorado = {1.25, 1.32, 0, 0};
+        double[] grupoAzul = {1.90, 0, 0, 0};
+        for (int i = 0; i < grupoDorado.length; i++) {
+            gestionador.agregarBoxeadoraGrupoDorado(grupoDorado[i]);
+            gestionador.agregarBoxeadoraGrupoAzul(grupoAzul[i]);
+        }
+        String esperado = "No";
+        String resultado = gestionador.ordenarBoxeadoras();
+        assertEquals(esperado, resultado);
+
+    }
     
 }
